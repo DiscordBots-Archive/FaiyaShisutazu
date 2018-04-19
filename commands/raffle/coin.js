381583ed6476dafe08054e5e55e5acd40defea59
Junwei Chen <jwchen1259@gmail.com>
2018-04-18 19:41:32 -0700

    Add coin flip command
    Signed-off-by: Junwei Chen <jwchen1259@gmail.com>

---
+++ commands/raffle/coin.js
@@ -0,0 +1,43 @@
+const Social = require(`${process.cwd()}/base/Social.js`);
+const fsn = require("fs-nextra");
+
+class Coin extends Social {
+  constructor(client) {
+    super(client, {
+      name: "coin",
+      description: "Flip a coin",
+      category: "5. Raffles & votes",
+      usage: "coin",
+      extended: "This command will flip a coin for you.",
+      cost: 2,
+      aliases: ["flip"],
+
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+      const heads = await fsn.readFile("./assets/images/heads.png");
+      const tails = await fsn.readFile("./assets/images/tails.png");
+      const number = Math.floor(Math.random() * 2) + 1;
+      if (number === 1) 
+        await message.channel.send({
+          files: [{
+            attachment: heads,
+            name: "heads.png"
+          }]
+        });
+      else
+        await message.channel.send({
+          files: [{
+            attachment: tails,
+            name: "tails.png"
+          }]
+        });
+    } catch (error) {
+      this.client.logger.error(error);
+    }
+  }
+}
+
+module.exports = Coin;
