a2eadb59bfe3d11afc4fd0e6f2d6c8b116e39eaa
Junwei Chen <jwchen1259@gmail.com>
2019-02-28 20:16:47 -0800

    Add missing files
    Signed-off-by: Junwei Chen <jwchen1259@gmail.com>

---
+++ assets/responses/karen.js
@@ -0,0 +1,74 @@
+// Karen's about/introduction 
+exports.aboutMessages = [
+  "Ohayō! I'm **Karen**!",
+  "**Karen**!",
+  "Not sure if this is the right moment... Anyway, I'm **Karen Araragi**.",
+  "**Karen**. We are the.. oh wait it's **Tsukihi-chan**'s turn.",
+  "Not sure if **Tsukihi-chan** will even reply to you but okay...I'm **Karen**.",
+  "Hm **Tsukihi-chan** might not want to do this ya know... Sigh, I'm Karen by the way."
+];
+
+// Member Join
+exports.welcomeMessages = [
+  "O-ohayou **{{user}}-san**.",
+  "O-ohayou gozaimasu **{{user}}-san**.",
+  "O-oh, welcome **{{user}}-san** to **{{guild}}**. I hope I haven't embarrased you...",
+  "Who dis? **{{user}}** s-sounds like a name for b-bakas...",
+  "Yay! More b-bakas in **{{guild}}** to deal with! Who is dis **{{user}}**!?",
+  "**Tsukihi-chan** I don't want to greet this **{{user}}**...",
+  "**{{user}}-san** ? U-uh did you get lost!?",
+  "I'm platinum mad! Are {{amount}} users in **{{guild}}** not enough already?! Why has **{{user}}** got to come?"
+];
+
+// Member Leave
+exports.goodbyeMessages = [
+  "Not joining **{{guild}}** again would be a very smart move **{{user}}-san** !",
+  "Awh, **{{user}}-san** has left... Just kidding, less people in **{{guild}}** to deal with!",
+  "Finally **{{user}}-san** left **{{guild}}**! Byeeeee~",
+  "Eeeee! **{{user}}-san** left?!",
+  "Not like we needed **{{user}}-san** in **{{guild}}** or anything, more space I guess?",
+  "Life finally freed me from **{{user}}**!"
+];
+
+// Karen's command check succeed
+exports.commandSuccessMessages = [
+  "Please wait a bit for **Tsukihi** to process **{{user}}-san**...",
+  "It's **{{user}}-san**, the big baka, again!",
+  "Can you help this baka out already **Tsukihi-chan**?",
+  "You can always ignore **{{user}}-san** **Tsukihi**!",
+  "Seems like you do know how to use a command **{{user}}-san** after all...",
+  "Running a command seems simple enough for a baka like **{{user}}-san**...",
+  "Here comes **{{user}}-san**, the big baka, **Tsukihi-chan**..."
+];
+
+// Karen's command check failed
+exports.commandErrorMessages = [
+  "Hmm.. Is this on purpose or is it just because **{{user}}-san** is retarded?! That command doesn't exist!",
+  "You can always run **{{prefix}}help** so you don't look like a baka next time **{{user}}-san**...",
+  "That command doesn't exist, run **{{prefix}}help** so you don't build up your reputation as a baka **{{user}}-san**.",
+  "It seems like you are a big baka **{{user}}-san**. Just run **{{prefix}}help** again please?!",
+  "This is getting pretty annoying...",
+  "This baka keeps misusing our prefix. Does it bother you **Tsukihi-chan**?",
+  "?? :smile: ?? **{{prefix}}help** is not that hard to remember..."
+];
+
+// Karen's timed out 
+exports.waitTsukihiMessages = [
+  "I don't see **Tsukihi-chan** responding...Maybe she fell asleep?",
+  "Where is **Tsukihi-chan**? Did you scare my little **Tsukihi** **{{user}}-san** away?!",
+  "Where are you **Tsukihi-chan**? Don't leave me alone with this big baka **{{user}}**!",
+  "Sigh... She is probably mad at you **{{user}}-san** since I don't see her response yet!",
+  "You should do this more oftens **Tsukihi-chan**! Just ignore **{{user}}-san**...",
+  "Looks like someone is platinum mad...",
+  "Seems like **{{user}}-san** is not worth **Tsukihi-chan**'s time!"
+];
+
+// Anime Steins;Gate Ref Correction
+exports.steinerMessages = [
+  "I'm assuming **{{user}}-san** means {{steiner}}???",
+  "Pretty sure it's \"{{steiner}}\" **{{user}}-san**...",
+  "I never knew **{{user}}-san** is such a big baka! It's \"{{steiner}}\"!",
+  "?? :smile: ?? It's \"{{steiner}}\" **{{user}}-san**!",
+  "\"{{steiner}}\"! **{{user}}-san**, do you you even Tutturu? https://i.imgur.com/N8n0ZC5.jpg",
+  "B-baka!!? It's \"{{steiner}}\"!"
+];
---
+++ structures/Karen.js
@@ -0,0 +1,116 @@
+const { Client } = require("discord.js");
+const CommandStore = require("./CommandStore.js");
+const EventStore = require("./EventStore.js");
+const BotConsole = require("./BotConsole.js");
+const Enmap = require("enmap");
+
+class Karen extends Client {
+  constructor(options) {
+    super(options);
+    this.config = require("../config.js");
+    this.console = new BotConsole(this);
+    this.responses = require("../assets/responses/karen.js");
+
+    this.commands = new CommandStore(this);
+    this.events = new EventStore(this);
+
+    this.levelCache = {};
+    this.methods = {
+      util: require("../util/Util.js"),
+      errors: require("../util/CustomError.js")
+    };
+
+    this.settings = new Enmap({ name: "settingsKaren" });
+
+    this.ready = false;
+    this.on("ready", this._ready.bind(this));
+    this.on("message", message => this._message(message));
+  }
+
+  _ready() {
+    this.ready = true;
+    this.emit("readyKaren");
+  }
+
+  _message(message) {
+    this.message = true;
+    this.emit("messageKaren", message);
+  }
+
+  async login(token) {
+    await this.init();
+    return super.login(token);
+  }
+    
+  get ping() {    
+    return this.pings.reduce((prev, p) => prev + p, 0) / this.pings.length;    
+  }
+
+  get status() {    
+    return this.ws.connection ? this.ws.connection.status : null;    
+  }
+
+  permlevel(message) {
+    let permlvl = 0;
+    
+    const permOrder = this.config.permLevels.slice(0).sort((prev, val) => prev.level < val.level ? 1 : -1);
+    
+    while (permOrder.length) {
+      const currentLevel = permOrder.shift();
+      if (message.guild && currentLevel.guildOnly) continue;
+      if (currentLevel.check(message)) {
+        permlvl = currentLevel.level;
+        break;
+      }
+    }
+    return permlvl;
+  }
+    
+  getGuildSettings(guild) {
+    const def = this.config.defaultSettings;
+    if (!guild) return def;
+    const returns = {};
+    const overrides = this.settings.get(guild.id) || {};
+    for (const key in def) {
+      returns[key] = overrides[key] || def[key];
+    }
+    return returns;
+  }
+    
+  getSettings(id) {
+    const defaults = this.settings.get("default") || this.config.defaultSettings;
+    let guild = this.settings.get(id);
+    if (typeof guild !== "object") guild = {};
+    const returnObject = {};
+    Object.keys(defaults).forEach(key => {
+      returnObject[key] = guild[key] ? guild[key] : defaults[key];
+    });
+    return returnObject;
+  }
+    
+  writeSettings(id, newSettings) {
+    const defaults = this.settings.get("default") || this.config.defaultSettings;
+    let settings = this.settings.get(id);
+    if (typeof settings !== "object") settings = {};
+    for (const key in newSettings) {
+      if (defaults[key] !== newSettings[key]) {
+        settings[key] = newSettings[key];
+      } else {
+        delete settings[key];
+      }
+    }
+    this.settings.set(id, settings);
+  }
+    
+  async init() {
+    const [commands, events] = await Promise.all([this.commands.loadFiles(), this.events.loadFiles()]);
+    this.console.log(`Karen loaded a total of ${commands} commands & ${events} events.`);
+    
+    for (let i = 0; i < this.config.permLevels.length; i++) {
+      const thisLevel = this.config.permLevels[i];
+      this.levelCache[thisLevel.name] = thisLevel.level;
+    }
+  }
+}
+
+module.exports = Karen;
