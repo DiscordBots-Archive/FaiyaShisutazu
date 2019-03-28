f03400ddaea2e4b4d79010abd5f4d42dd2295360
Junwei Chen <jwchen1259@gmail.com>
2019-03-28 12:57:54 -0700

    Alternate activity statuses every 5s
    Signed-off-by: Junwei Chen <jwchen1259@gmail.com>

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,27 @@
 const Event = require("../structures/Event.js");
+const activities = [
+  "with {{users}} users",
+  "over {{servers}} servers",
+  "@{{ping}} help"
+];
 
 module.exports = class extends Event {
 
   async run() {
     if (this.client.users.has("1")) this.client.users.delete("1");
 
-    this.client.user.setActivity(`@${this.client.user.username} help`);
+    let count = 0;
+
+    setInterval(() => {
+      if (count++ === 0)
+        this.client.user.setActivity(activities[count].replaceAll("{{users}}", this.client.users.size));
+      else if (count++ === 1)
+        this.client.user.setActivity(activities[count].replaceAll("{{servers}}", this.client.guilds.size), { type: "WATCHING" });
+      else {
+        this.client.user.setActivity(activities[count].replaceAll("{{ping}}", this.client.user.username), { type: "LISTENING" });
+        count = 0;
+      }
+    }, 5000);
 
     this.client.console.log(`${this.client.user.tag}, ready to serve ${this.client.users.size} users in ${this.client.guilds.size} servers.`);
     
